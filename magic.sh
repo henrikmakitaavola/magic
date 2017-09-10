d889e361de927a5ab4355c6f5b09c803d9159fc8
Henrik Mäkitaavola <henrik.makitaavola@gmail.com>
2017-09-10 09:55:48 +0200

    Format for pastebin

--- magic.sh
+++ magic.sh
@@ -1,5 +1,2 @@
-#!/bin/sh
-cp /home/root/.ssh/id_rsa.public /tmp/key
-sed -i -e 's/\//APA/g' /tmp/key
-sed -i -e 's/\+/HAST/g' /tmp/key
-sed -i -e 's/=/OSNA/g' /tmp/key
+# Use pastebin to wget it to GW.
+cp /home/root/.ssh/id_rsa.public /tmp/key ; sed -i -e 's/\//APA/g' /tmp/key ; sed -i -e 's/\+/HAST/g' /tmp/key ; sed -i -e 's/=/OSNA/g' /tmp/key
