1164e4449b07d30ed7b4620f28da87797beba17e
Henrik Mäkitaavola <henrik.makitaavola@gmail.com>
2017-09-10 10:00:26 +0200

    Added comments

--- magic.sh
+++ magic.sh
@@ -1,2 +1,2 @@
-# Use pastebin to wget it to GW.
+# Use pastebin to wget it to GW. (wget http://pastebin.com/raw/<id> -O /tmp/magic.sh) Note that https is not supported on GW.
 cp /home/root/.ssh/id_rsa.public /tmp/key ; sed -i -e 's/\//APA/g' /tmp/key ; sed -i -e 's/\+/HAST/g' /tmp/key ; sed -i -e 's/=/OSNA/g' /tmp/key
